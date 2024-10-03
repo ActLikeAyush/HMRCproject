930b6868834ec770ec744c12869ef3ece990ce03
ayush pathak <ayushpathak1975@gmail.com>
2024-10-04 01:33:14 +0530

    make some change in header added signup button and some logic

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -16,4 +16,19 @@ export class HeaderComponent {
   onWindowScroll() {
     this.isScrolled = window.scrollY > 80; // Check if the user has scrolled more than 80px
   }
+   
+  isModalOpen = false;
+
+  // Detect scrolling
+  onScroll(event: Event) {
+    this.isScrolled = window.scrollY > 0;
+  }
+
+  openSignupModal() {
+    this.isModalOpen = true;
+  }
+
+  closeSignupModal() {
+    this.isModalOpen = false;
+  }
 }
